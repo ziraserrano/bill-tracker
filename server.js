f7b237146ea569d64cb9d3712747e5e7dd153340
zira serrano <ziraserrano.dev@gmail.com>
2023-06-11 13:52:49 -0400

    ..

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-let PORT = 3000
+const PORT = process.env.PORT || 3000
 const cors = require("cors")
 
 require('dotenv').config()
@@ -51,6 +51,6 @@ app.delete('/deleteBill', (req, res) => {
 })
 
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
