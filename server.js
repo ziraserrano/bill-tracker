fda9f3262e3c741c8c7d1892b3069e25e32861a9
zira serrano <ziraserrano.dev@gmail.com>
2023-06-18 18:33:06 -0400

    Delete cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = process.env.PORT || 3000
-const cors = require("cors")
+
 
 require('dotenv').config()
 
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
+
 
 
 app.get('/', (req, res) => {
